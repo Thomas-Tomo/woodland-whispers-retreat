1266793e106bc23fb8d9c2c2d004d4d9d8b31372
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-13 18:33:24 +0000

    Add SECRET_KEY variable to settings.py

--- woodland_retreat/settings.py
+++ woodland_retreat/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_mqcuv#er+$oc=%mts=@a&t3-!bcc@yj*oi3g%65wjg^j_317c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
