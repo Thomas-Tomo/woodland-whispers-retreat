d13b29b3e2467ad399cd94078cf802f88a8e1ca8
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-13 18:36:05 +0000

    Add connection to database

--- woodland_retreat/settings.py
+++ woodland_retreat/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'woodland_retreat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 
 # Password validation
