7d1edbf8913606a672f8e4ff6663a800b7ea4b52
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-16 16:45:36 +0000

    Add view and url for base.html

--- cabin_bookings/views.py
+++ cabin_bookings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def base_view(request):
+    return render(request, 'base.html')
--- woodland_retreat/urls.py
+++ woodland_retreat/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cabin_bookings.views import base_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', base_view, name='base'),
 ]
