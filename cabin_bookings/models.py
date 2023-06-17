2187c20774ac92e71cd7afd2172887e83b430f94
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-17 11:30:49 +0000

    Add Cabin model to models.py

--- cabin_bookings/models.py
+++ cabin_bookings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Cabin(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = CloudinaryField('log_cabin_images')
+
+    def __str__(self):
+        return self.name
