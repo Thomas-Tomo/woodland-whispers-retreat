9f87476ca7cfb4d1e4c4514c11244e0e096ec1cb
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-20 17:11:36 +0000

    Add path and view for cabin_booking template

--- cabin_bookings/views.py
+++ cabin_bookings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.core.paginator import Paginator
 from django.views import generic, View
 from .models import Cabin
 
@@ -13,3 +14,12 @@ class Home(generic.TemplateView):
 def cabin_list(request):
     cabins = Cabin.objects.all()
     return render(request, 'index.html', {'cabins': cabins})
+
+
+def cabin_booking(request):
+    all_cabins = Cabin.objects.all()
+    paginator = Paginator(all_cabins, 6)  # Display 6 cabins per page
+    page_number = request.GET.get('page')
+    cabins = paginator.get_page(page_number)
+
+    return render(request, 'cabin_booking.html', {'cabins': cabins})
