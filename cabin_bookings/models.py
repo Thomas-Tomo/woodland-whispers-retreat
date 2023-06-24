01f431588035965ab7dd4984862f143b0c4119b0
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-24 12:52:04 +0000

    Update num_guests field to max-guests in Cabin model

--- cabin_bookings/models.py
+++ cabin_bookings/models.py
@@ -10,7 +10,7 @@ class Cabin(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = CloudinaryField('log_cabin_images')
-    num_guests = models.PositiveIntegerField(default=1)
+    max_guests = models.PositiveIntegerField(default=1)
     bedrooms = models.PositiveIntegerField(default=1)
 
     def __str__(self):
