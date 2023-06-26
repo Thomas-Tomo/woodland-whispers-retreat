0aedcd45b6ab955209673d0fb239792e65ce2354
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-06-26 16:07:51 +0000

    Add Booking to admin.site.register

--- cabin_bookings/admin.py
+++ cabin_bookings/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Cabin
+from .models import Cabin, Booking
 
 # Register your models here.
 
 admin.site.register(Cabin)
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'cabin', 'check_in_date', 'check_out_date']
+
+
+admin.site.register(Booking, BookingAdmin)
