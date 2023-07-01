f23bc81c4d7ded976d7a6af35ee3710ac3ba129e
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-01 19:58:40 +0000

    Fix validation with bookings with 0 guests

--- cabin_bookings/forms.py
+++ cabin_bookings/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Booking
+from django.core.validators import MinValueValidator
 
 
 class BookingForm(forms.ModelForm):
+    num_guests = forms.IntegerField(validators=[MinValueValidator(1)])
+
     class Meta:
         model = Booking
         fields = ['check_in_date', 'check_out_date', 'num_guests']
