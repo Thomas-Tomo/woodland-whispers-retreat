e2c22699b0ca0ef986736d99ee150231898b1ddd
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-01 16:47:54 +0000

    Update ValidationErrors in Booking model

--- cabin_bookings/models.py
+++ cabin_bookings/models.py
@@ -34,3 +34,8 @@ class Booking(models.Model):
             raise ValidationError('Please select a future check-in date.')
         if self.check_out_date < self.check_in_date:
             raise ValidationError('Check-out date cannot be earlier than the check-in date.')  # noqa
+        if self.check_in_date and self.check_out_date:
+            if self.check_in_date >= self.check_out_date:
+                raise ValidationError("Check-in date must be before check-out date.")  # noqa
+        else:
+            raise ValidationError("Both check-in and check-out dates are required.")  # noqa
