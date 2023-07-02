e5d793705d69e8e7395cce11bb9dc72a049eacef
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-02 18:21:19 +0000

    Update ACCOUNT_EMAIL_VERIFICATION to none

--- woodland_retreat/settings.py
+++ woodland_retreat/settings.py
@@ -62,6 +62,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
