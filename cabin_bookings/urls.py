7b3180d9811acbed2a877c215f70f90b017cd5bb
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-05 20:08:40 +0000

    Add line breaks to url.py for pep8 validation

--- cabin_bookings/urls.py
+++ cabin_bookings/urls.py
@@ -5,10 +5,15 @@ from django.urls import path
 urlpatterns = [
     path('', views.cabin_list, name='home'),
     path('cabin-booking/', views.cabin_booking, name='cabin_booking'),
-    path('booking/<int:cabin_id>/', views.booking_create, name='my_booking'),  # noqa
-    path('booking/success/<int:cabin_id>/<int:booking_id>/', views.booking_success, name='booking_success'),  # noqa
-    path('booking-overview/', views.booking_overview, name='booking_overview'),
-    path('booking/edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),  # noqa
-    path('booking/<int:booking_id>/delete/', views.delete_booking, name='delete_booking'),  # noqa
+    path('booking/<int:cabin_id>/',
+         views.booking_create, name='my_booking'),
+    path('booking/success/<int:cabin_id>/<int:booking_id>/',
+         views.booking_success, name='booking_success'),
+    path('booking-overview/',
+         views.booking_overview, name='booking_overview'),
+    path('booking/edit/<int:booking_id>/',
+         views.edit_booking, name='edit_booking'),
+    path('booking/<int:booking_id>/delete/',
+         views.delete_booking, name='delete_booking'),
     path('contact/', views.contact_view, name='contact'),
 ]
