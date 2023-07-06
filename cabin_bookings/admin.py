321a3abf97046ed1643a6e385157f35d92dbf871
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-06 18:34:31 +0000

    Add Amenity model to admin.py

--- cabin_bookings/admin.py
+++ cabin_bookings/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Cabin, Booking
+from .models import Cabin, Booking, Amenity
 
 # Register your models here.
 
+admin.site.register(Amenity)
 admin.site.register(Cabin)
 
 
