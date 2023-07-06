0c79f4660f196dac7d70a554a25c4dcab21aa6ea
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-06 18:36:18 +0000

    Update forms.py with amenities fields

--- cabin_bookings/forms.py
+++ cabin_bookings/forms.py
@@ -1,11 +1,20 @@
 from django import forms
-from .models import Booking
+from .models import Booking, Amenity
 from django.core.validators import MinValueValidator
 
 
 class BookingForm(forms.ModelForm):
     num_guests = forms.IntegerField(validators=[MinValueValidator(1)])
+    cave_exploration_tickets = forms.IntegerField(
+                               validators=[MinValueValidator(0)],
+                               required=False)
+    kayak_rentals = forms.IntegerField(validators=[MinValueValidator(0)],
+                                       required=False)
 
     class Meta:
         model = Booking
-        fields = ['check_in_date', 'check_out_date', 'num_guests']
+        fields = ['check_in_date',
+                  'check_out_date',
+                  'num_guests',
+                  'cave_exploration_tickets',
+                  'kayak_rentals']
