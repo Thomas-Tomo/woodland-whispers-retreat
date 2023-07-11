6221a062915b82bc8f667b656b5eb74b02716bf8
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-11 18:52:40 +0000

    Update admin interface with additional fields

--- cabin_bookings/admin.py
+++ cabin_bookings/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from .models import Cabin, Booking, Amenity
 
-# Register your models here.
-
-admin.site.register(Amenity)
-admin.site.register(Cabin)
-
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'cabin', 'check_in_date', 'check_out_date']
+    """
+    Admin configuration for the Booking model.
+
+    This class defines the custom admin interface for the Booking model.
+
+    It specifies the list of fields to be displayed in the admin list
+    view for Booking objects.
+    """
+    list_display = ['id',
+                    'user',
+                    'cabin',
+                    'check_in_date',
+                    'check_out_date',
+                    'cave_exploration_tickets',
+                    'kayak_rentals']
 
 
+# Registering the models in the admin site
+admin.site.register(Amenity)
+admin.site.register(Cabin)
 admin.site.register(Booking, BookingAdmin)
