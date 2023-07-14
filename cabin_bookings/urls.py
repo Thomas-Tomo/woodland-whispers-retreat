f4a6bfb53f11d7101a8cec51fa9e5cd079d0db7a
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-07-14 17:41:22 +0000

    Add 500 error handler

--- cabin_bookings/urls.py
+++ cabin_bookings/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
 from django.conf.urls import handler404
-from .views import custom_handler404
+from django.conf.urls import handler500
+from .views import custom_handler404, custom_handler500
 
 # Url Mapping
 urlpatterns = [
@@ -24,3 +25,4 @@ urlpatterns = [
 ]
 
 handler404 = custom_handler404
+handler500 = custom_handler500
--- cabin_bookings/views.py
+++ cabin_bookings/views.py
@@ -447,4 +447,14 @@ def delete_booking(request, booking_id):
 
 
 def custom_handler404(request, exception):
+    """
+    Custom handler for 404 (Page Not Found) errors.
+    """
     return render(request, '404.html', status=404)
+
+
+def custom_handler500(request):
+    """
+    Custom handler for 500 (Internal Server Error) errors.
+    """
+    return render(request, '500.html', status=500)
